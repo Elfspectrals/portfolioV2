5fa0c5c28433dbfcacc3f58b3451aae427305334
Jerome <jerome.neupert@epitech.eu>
2024-08-01 15:39:51 +0200

    add description

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,79 @@
-import Escape from './Assets/hacker.jpg'
-import Secret from './Assets/secret.png'
-import Design from './Assets/design.png'
-import Zombie from './Assets/zombie.png'
-import './App.css'
-export default function Component() {
+import React, { useState } from 'react';
+import Escape from './Assets/hacker.jpg';
+import Secret from './Assets/secret.png';
+import Design from './Assets/design.png';
+import Zombie from './Assets/zombie.png';
+import './App.css';
+
+export default function App () {
+  const [selectedProject, setSelectedProject] = useState('default');
+
+  const renderProjectDetails = () => {
+    switch (selectedProject) {
+      case 'Neo Jerome':
+        return (
+          <div className="p-5 bg-gray-800 rounded-lg mt-5">
+            <p className="text-gray-300">
+              Description : The Hacking Traitor <br />
+              Envoyé par la compagnie <i>Neo Jerome</i> pour trouver leur traître, <br />
+              Il y aura donc plusieurs phase de jeu : <br />
+              - La phase de hacking des informations de la compagnie <br />
+              - La phase de recherche <br />
+            </p>
+            <div className="mt-2">
+              <h3 className="text-yellow-400">Technologies utilisées :</h3>
+              <li className="list-none mt-1">React <img src={Zombie} alt='' className="inline-block w-5 h-5 ml-2"/></li>
+            </div>
+          </div>
+        );
+      case 'Jerome Chanel':
+        return (
+          <div className="p-5 bg-gray-800 rounded-lg mt-5">
+            <p className="text-gray-300">
+              Description : Chanel Jérôme <br />
+              The ability to design your own outfit <br />
+            </p>
+            <div className="mt-2">
+              <h3 className="text-yellow-400">Technologies utilisées :</h3>
+              <ul>
+                <li className="list-none mt-1">React :<img src={Zombie} alt='' className="inline-block w-12 h-12 ml-2"/></li>
+                <li className="list-none mt-1">React :<img src={Zombie} alt='' className="inline-block w-12 h-12 ml-2"/></li>
+                <li className="list-none mt-1">React :<img src={Zombie} alt='' className="inline-block w-12 h-12 ml-2"/></li>
+                <li className="list-none mt-1">React :<img src={Zombie} alt='' className="inline-block w-12 h-12 ml-2"/></li>
+
+              </ul>
+            </div>
+          </div>
+        );
+      default:
+        return (
+          <div className="p-5 bg-gray-800 rounded-lg mt-5">
+            <p className="text-gray-300">Select a project to see the details.</p>
+          </div>
+        );
+    }
+  };
+
   return (
-    <div className="bg-black text-white w-full h-screen flex justify-center items-center">
+    <div className="bg-black text-white w-full h-screen flex flex-col justify-center items-center">
       <section className="mb-8 w-full max-w-screen-lg mx-auto">
-        <h2 className="text-4xl font-bold mb-4 text-center text-indigo-400 ">Creation of Jerome</h2>
+        <h2 className="text-4xl font-bold mb-4 text-center text-indigo-400">Creation of Jerome</h2>
         <div className="flex space-x-4">
-          <div className="w-64 flex-shrink-0">
+          <div className="w-64 flex-shrink-0" onClick={() => setSelectedProject('Neo Jerome')}>
             <img src={Escape} alt="The Crown" className="w-full h-64 object-cover" />
             <div className="mt-2 text-center">
               <p className="text-2xl text-emerald-400">Neo Jerome</p>
               <a href="https://escapegame-9f1f3.web.app/">
-                <p className="font-bold text-sky-400 bold underline animate-pulse duration-75">The Hacking Traitor</p>
-              </a>            
-              </div>
+                <p className="font-bold text-sky-400 underline animate-pulse duration-75">The Hacking Traitor</p>
+              </a>
+            </div>
           </div>
-          <div className="w-64 flex-shrink-0">
+          <div className="w-64 flex-shrink-0" onClick={() => setSelectedProject('Jerome Chanel')}>
             <img src={Design} alt="Stranger Things" className="w-full h-64 object-cover" />
             <div className="mt-2 text-center">
               <p className="text-2xl text-orange-400">Jerome Chanel</p>
               <a href="https://designer-97084.web.app/">
-              <p className="font-bold text-yellow-400 bold underline animate-bounce duration-75">Made in Jérôme</p>
+                <p className="font-bold text-yellow-400 underline animate-bounce duration-75">Made in Jérôme</p>
               </a>
             </div>
           </div>
@@ -33,7 +83,7 @@ export default function Component() {
               <p className="text-sm">CIA</p>
               <p className="font-bold">Secret project [Jeux Vidéo]</p>
               <p>Unreal Engine 5 🤫 <br />
-              [ Il arrive bientôt promis ! ]</p>
+                [ Il arrive bientôt promis ! ]</p>
             </div>
           </div>
           <div className="w-64 flex-shrink-0">
@@ -43,9 +93,16 @@ export default function Component() {
               <p className="font-bold">Secret project</p>
             </div>
           </div>
-
         </div>
       </section>
+      <div className="w-full max-w-screen-lg mx-auto">
+        <div>
+          <h2 className="text-4xl font-bold mb-4 text-center text-indigo-400">Learn about any project [click on the image]</h2>
+          <div className="text-center text-lg">
+            {renderProjectDetails()}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
