759d56ee18e86d4d9146806543b30363790dedbf
Jerome <jerome.neupert@epitech.eu>
2025-05-13 20:32:14 +0200

    ok cool

--- portfolio/src/ThreeJS.jsx
+++ portfolio/src/ThreeJS.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-// import Scene from './Scene';
 import { Scene } from './Scene'; // Correct import for named export
 import { OrbitControls } from '@react-three/drei';
+import * as dat from 'dat.gui';
 
-const App = () => {
+const ThreeJS = () => {
+
+
+
   return (
     <Canvas>
       <ambientLight />
       <OrbitControls />
       <pointLight position={[10, 10, 10]} />
+      {/* Passez la scale en prop au composant Scene */}
       <Scene />
     </Canvas>
   );
 };
 
-export default App;
+export default ThreeJS;
