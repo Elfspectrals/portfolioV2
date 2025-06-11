aa2eb6fabf73b0a85fc6b89f509b3690f7be80ef
Elfspectrals <jerome.neupert@epitech.eu>
2025-06-11 10:28:19 +0200

    rework of 3d stuff

--- src/components/ThreeDImage/ThreeDImage.tsx
+++ src/components/ThreeDImage/ThreeDImage.tsx
@@ -1,39 +1,45 @@
+// components/ThreeDImage/ThreeDImage.tsx
 import React, { useRef } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { useFrame, useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
-import image from '../../assets/react.svg';
-import styles from './ThreeDImage.module.scss';
 
-const TexturedBox: React.FC = () => {
+interface ThreeDImageProps {
+  textureImage: string;
+  shape: 'box' | 'circle' | 'rectangle';
+  position?: [number, number, number];
+}
+
+const ThreeDImage: React.FC<ThreeDImageProps> = ({
+  textureImage,
+  shape,
+  position = [0, 0, 0]
+}) => {
   const meshRef = useRef<THREE.Mesh>(null!);
-  const texture = useLoader(THREE.TextureLoader, image);
+  const texture = useLoader(THREE.TextureLoader, textureImage);
 
-  useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta * 0.5;
-      meshRef.current.rotation.y += delta * 0.5;
-    }
+  useFrame((_, delta) => {
+    meshRef.current.rotation.x += delta * 0.5;
+    meshRef.current.rotation.y += delta * 0.5;
   });
 
+  let geometry;
+  switch (shape) {
+    case 'box':
+      geometry = <boxGeometry args={[1.5, 1.5, 1.5]} />;
+      break;
+    case 'circle':
+      geometry = <circleGeometry args={[1.5, 32]} />;
+      break;
+    case 'rectangle':
+      geometry = <boxGeometry args={[2, 1, 1]} />;
+      break;
+  }
+
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1.5, 1.5, 1.5]} />
+    <mesh ref={meshRef} position={position}>
+      {geometry}
       <meshStandardMaterial map={texture} />
     </mesh>
-  );
-};
-
-export const ThreeDImage: React.FC = () => {
-  return (
-    <div className={styles.container}>
-      <Canvas>
-        <ambientLight intensity={0.3} />
-        <pointLight position={[5, 5, 5]} intensity={0.8} />
-        <TexturedBox />
-        <OrbitControls />
-      </Canvas>
-    </div>
   );
 };
 
--- src/pages/3DWorld.tsx
+++ src/pages/3DWorld.tsx
@@ -1,8 +1,37 @@
+// pages/ThreeDWorld.tsx (or wherever you mount it)
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import ThreeDImage from '../components/ThreeDImage/ThreeDImage';
+import image from '../assets/react.svg';
 
-const ThreeDWorld: React.FC = () => {
-  return <ThreeDImage />;
-};
+const shapes: Array<{ shape: 'box' | 'circle' | 'rectangle'; position: [number, number, number] }> = [
+  { shape: 'box',       position: [ -2, 0, 0 ] },
+  { shape: 'circle',    position: [  0, 0, 0 ] },
+  { shape: 'rectangle', position: [  2, 0, 0 ] },
+];
+
+const ThreeDWorld: React.FC = () => (
+  <div style={{ width: '100vw', height: '100vh' }}>
+    <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
+      {/* lights */}
+      <ambientLight intensity={0.3} />
+      <pointLight position={[5, 5, 5]} intensity={0.8} />
+
+      {/* orbit controls for interaction */}
+      <OrbitControls enablePan={true} enableZoom={true} />
+
+      {/* inject your meshes */}
+      {shapes.map(({ shape, position }, idx) => (
+        <ThreeDImage
+          key={idx}
+          textureImage={image}
+          shape={shape}
+          position={position}
+        />
+      ))}
+    </Canvas>
+  </div>
+);
 
 export default ThreeDWorld;
